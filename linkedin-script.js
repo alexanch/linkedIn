a11cc74030d95df8882865b50a983dbd7858892a
alexanch <alexboyko94@gmail.com>
2020-05-03 02:04:15 +0200

    Create linkedin-script.js

---
+++ linkedin-script.js
@@ -0,0 +1,2 @@
+document.querySelector("button.pv-profile-section__card-action-bar").click()
+document.querySelectorAll(".pv-profile-section.pv-skill-categories-section.artdeco-container-card.first-degree.ember-view button").forEach(item => item.click())
