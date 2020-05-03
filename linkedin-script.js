00c979ec9c190f439979741057b1b3934796769e
alexanch <alexboyko94@gmail.com>
2020-05-03 02:05:20 +0200

    Update linkedin-script.js

--- linkedin-script.js
+++ linkedin-script.js
@@ -1,2 +1,4 @@
+// copy and paste the script to Console:
+
 document.querySelector("button.pv-profile-section__card-action-bar").click()
 document.querySelectorAll(".pv-profile-section.pv-skill-categories-section.artdeco-container-card.first-degree.ember-view button").forEach(item => item.click())
